a13fa26ad5d66c785c528fa520e87058e3498044
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-01-27 00:29:25 +0530

    AVL-Tree

---
+++ src/data-structure/trees/avl-tree/__tests__/avlTree.test.js
@@ -0,0 +1,198 @@
+const AvlTree = require('../avlTree')
+
+describe("create AvlTree", () => {
+  it('should create empty AVL tree', function () {
+    const tree = new AvlTree()
+
+    expect(tree).toBeDefined()
+    expect(tree.root).toBeDefined()
+    expect(tree.root.left).toBeNull()
+    expect(tree.root.right).toBeNull()
+  });
+
+  it('should do left-left rotation', function () {
+    const tree = new AvlTree()
+
+    tree.insert(10)
+    tree.insert(9)
+    tree.insert(8)
+    tree.insert(7)
+
+    let s = ""
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("7,8,9,10")
+    expect(s).toBe("9,8,7,10,")
+    checkParents(tree.root)
+
+    tree.insert(6)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("6,7,8,9,10")
+    expect(s).toBe("9,7,6,8,10,")
+    checkParents(tree.root)
+
+    tree.insert(5)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("5,6,7,8,9,10")
+    expect(s).toBe("7,6,5,9,8,10,")
+    checkParents(tree.root)
+
+    tree.insert(4)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("4,5,6,7,8,9,10")
+    expect(s).toBe("7,5,4,6,9,8,10,")
+    checkParents(tree.root)
+
+  });
+
+  it('should do right-right rotation', function () {
+    const tree = new AvlTree()
+
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+
+    tree.insert(5)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("1,2,3,4,5")
+    expect(s).toBe("2,1,4,3,5,")
+    checkParents(tree.root)
+
+    tree.insert(6)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("1,2,3,4,5,6")
+    expect(s).toBe("4,2,1,3,5,6,")
+    checkParents(tree.root)
+
+    tree.insert(7)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("1,2,3,4,5,6,7")
+    expect(s).toBe("4,2,1,3,6,5,7,")
+    checkParents(tree.root)
+
+  });
+
+  it('should do left-right rotation', function () {
+    const tree = new AvlTree()
+
+    tree.insert(200)
+    tree.insert(100)
+    tree.insert(150)
+
+    let s = ""
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,150,200")
+    expect(s).toBe("150,100,200,")
+    checkParents(tree.root)
+
+    tree.insert(105)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,105,150,200")
+    expect(s).toBe("150,100,105,200,")
+    checkParents(tree.root)
+
+    tree.insert(110)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,105,110,150,200")
+    expect(s).toBe("150,105,100,110,200,")
+    checkParents(tree.root)
+
+    tree.insert(108)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,105,108,110,150,200")
+    expect(s).toBe("110,105,100,108,150,200,")
+    checkParents(tree.root)
+
+    tree.insert(106)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,105,106,108,110,150,200")
+    expect(s).toBe("110,105,100,108,106,150,200,")
+    checkParents(tree.root)
+
+    tree.insert(107)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,105,106,107,108,110,150,200")
+    expect(s).toBe("110,105,100,107,106,108,150,200,")
+    checkParents(tree.root)
+
+    tree.insert(95)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("95,100,105,106,107,108,110,150,200")
+    expect(s).toBe("110,105,100,95,107,106,108,150,200,")
+    checkParents(tree.root)
+
+    tree.insert(109)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("95,100,105,106,107,108,109,110,150,200")
+    expect(s).toBe("107,105,100,95,106,110,108,109,150,200,")
+    checkParents(tree.root)
+  });
+
+  it('should do right-left rotation', function () {
+    const tree = new AvlTree()
+
+    tree.insert(100)
+    tree.insert(200)
+    tree.insert(500)
+
+    let s = ""
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,500")
+    expect(s).toBe("200,100,500,")
+    checkParents(tree.root)
+
+    tree.insert(400)
+    tree.insert(225)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,225,400,500")
+    expect(s).toBe("200,100,400,225,500,")
+    checkParents(tree.root)
+
+    tree.insert(210)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,210,225,400,500")
+    expect(s).toBe("225,200,100,210,400,500,")
+    checkParents(tree.root)
+
+    tree.insert(325)
+    tree.insert(310)
+    tree.insert(350)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,210,225,310,325,350,400,500")
+    expect(s).toBe("225,200,100,210,400,325,310,350,500,")
+    checkParents(tree.root)
+
+    tree.insert(340)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,210,225,310,325,340,350,400,500")
+    expect(s).toBe("225,200,100,210,350,325,310,340,400,500,")
+    checkParents(tree.root)
+
+    tree.insert(330)
+    s = preOrderString(tree)
+    expect(tree.toString()).toBe("100,200,210,225,310,325,330,340,350,400,500")
+    expect(s).toBe("325,225,200,100,210,310,350,340,330,400,500,")
+    checkParents(tree.root)
+  });
+})
+
+
+function preOrderString(tree) {
+  let s = ""
+  tree.traversePreOrder((a) => {
+    s += a.value + ","
+  })
+  return s
+}
+
+function checkParents(node, v = 0) {
+  if (v) expect(node.parent).not.toBeNull()
+  // try {
+  //   if(v) expect(node.parent).not.toBeNull()
+  // }
+  // catch (e) {
+  //   console.log(node.value)
+  // }
+  if (node.left) checkParents(node.left, 1)
+  if (node.right) checkParents(node.right, 1)
+}
---
+++ src/data-structure/trees/avl-tree/avlTree.js
@@ -0,0 +1,134 @@
+const BinarySearchTree = require('../binarySearchTree/binarySearchTree')
+
+class AvlTree extends BinarySearchTree {
+  constructor(value) {
+    super(value)
+  }
+
+  insert(value) {
+    super.insert(value)
+
+    let node = this.root.find(value)
+    while (node) {
+      this.balance(node)
+      node = node.parent
+    }
+  }
+
+  remove(value) {
+    let removed = super.remove(value)
+    if (removed) this.balance(this.root)
+    return removed
+  }
+
+  balance(node) {
+    if (node.balanceFactor > 1) {
+      if (node.left.balanceFactor > 0) {
+        // LL case
+        this.rotateLL(node)
+      } else {
+        // LR case
+        this.rotateLR(node)
+      }
+    } else if (node.balanceFactor < -1) {
+      if (node.right.balanceFactor > 0) {
+        // RL case
+        this.rotateRL(node)
+      } else {
+        // RR case
+        this.rotateRR(node)
+      }
+    }
+  }
+
+  /**
+   * @param node {BinarySearchTreeNode}
+   */
+  rotateLL(node) {
+    let parent = node.parent
+    let leftOfNode = node.left
+    let rightOfLeftOfNode = leftOfNode.right
+    leftOfNode.right = null
+    node.left = rightOfLeftOfNode
+
+    if (parent) {
+      if (parent.value > node.value) parent.left = leftOfNode
+      else parent.right = leftOfNode
+    } else {
+      this.root = leftOfNode
+      leftOfNode.parent = null
+    }
+    leftOfNode.right = node
+  }
+
+  /**
+   * @param node {BinarySearchTreeNode}
+   */
+  rotateLR(node) {
+    let left = node.left
+    let rightOfLeft = left.right
+    let leftOfRightOfLeft = rightOfLeft.left
+    rightOfLeft.left = null
+    left.right = leftOfRightOfLeft
+    node.left = rightOfLeft
+    rightOfLeft.left = left
+    this.rotateLL(node)
+  }
+
+  /**
+   * @param node {BinarySearchTreeNode}
+   */
+  rotateRR(node) {
+    let parent = node.parent
+    let rightNode = node.right
+    let leftOfRightNode = rightNode.left
+    rightNode.left = null
+    node.right = leftOfRightNode
+
+    if (parent) {
+      if (node.value < parent.value) parent.left = rightNode
+      else parent.right = rightNode
+    } else {
+      this.root = rightNode
+      rightNode.parent = null
+    }
+
+    rightNode.left = null
+    rightNode.left = node
+  }
+
+  /**
+   * @param node {BinarySearchTreeNode}
+   */
+  rotateRL(node) {
+    let right = node.right
+    let leftOfRight = right.left
+    let rightOfLeftOfRight = leftOfRight.right
+    leftOfRight.right = null
+    right.left = rightOfLeftOfRight
+    node.right = leftOfRight
+    leftOfRight.right = right
+
+    this.rotateRR(node)
+  }
+
+  static fromArray(array) {
+    const tree = new AvlTree()
+    array.sort((p, q) => p - q)
+
+    i(0, array.length - 1)
+
+    function i(l, r) {
+      if (l > r) return
+      let m = Math.floor((l + r) / 2)
+      tree.insert(array[m])
+      i(l, m - 1)
+      i(m + 1, r)
+    }
+
+    return tree
+  }
+
+}
+
+module.exports = AvlTree
